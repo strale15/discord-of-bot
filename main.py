49a531d4ec34c31132b50d13dc874fd694006d80
ERAKOVIC, Strahinja (EXT) <strahinja.erakovic@panonit.com>
2025-01-21 12:52:02 +0100

    Basic log configured

--- main.py
+++ main.py
@@ -1,50 +1,57 @@
+import settings
 import discord
 from discord.ext import commands
 from discord import app_commands
-from dotenv import load_dotenv
-import os
-
-# Load environment variables from .env file
-load_dotenv()
-
-BOT_TOKEN = os.environ.get("TOKEN")
-GUILD_ID = os.environ.get("GUILD_ID")
 
 class MyClient(commands.Bot):
     async def on_ready(self):
-        print(f'Logged on as {self.user}!')
+        log.info(f'Logged on as {self.user}!')
         
         try:
-            synced = await self.tree.sync(guild=SERVER_ID)
-            print(f'Synced {len(synced)} commands!')        
+            synced = await self.tree.sync(guild=settings.GUILD_ID)
+            log.info(f'Synced {len(synced)} commands!')        
         except Exception as e:
-            print(f"Sync failed {e}")
+            log.info(f"Sync failed {e}")
 
     async def on_message(self, message):   
         if message.author == self.user:
             return
         #Does something when message is typed
         #await message.channel.send("message")
-        
-#Bot setup
-SERVER_ID = discord.Object(id=GUILD_ID)
-
-intents = discord.Intents.all()
+        #log.info(f'Message was sent: {message}')
+     
+#SETUP BOT
+log = settings.logging.getLogger()
+    
+intents = discord.Intents.default()
 intents.message_content = True
 
 client = MyClient(command_prefix="!", intents=intents)
 
+#Global error handling        
+@client.tree.error
+async def on_error(interaction: discord.Interaction, error: discord.app_commands.AppCommandError) -> None:
+    log.error(f"Error type: {type(error)}")
+    if isinstance(error, app_commands.errors.MissingPermissions):
+        await interaction.response.send_message('You do not have a permission.')
+    else:
+        await interaction.response.send_message(f'Error global: ({error})')
+
 #Commands
-@client.tree.command(name="test", description="Does something", guild=SERVER_ID)
+@client.tree.command(name="test", description="Does something", guild=settings.GUILD_ID)
 async def executeCommand(interaction: discord.Interaction):
     await interaction.response.send_message("HI!")
     
-@client.tree.command(name="param", description="Does something but with param", guild=SERVER_ID)
+@client.tree.command(name="param", description="Does something but with param", guild=settings.GUILD_ID)
 async def executeCommand(interaction: discord.Interaction, number: int):
     await interaction.response.send_message(f"You entered: {number}")
+    
+@client.tree.command(name="raise-error", description="Raises error", guild=settings.GUILD_ID)
+async def raiseErrorCommand(interaction: discord.Interaction):
+    raise discord.ext.commands.CommandError("Test error")
 
 '''
-@client.tree.command(name="setup", description="Create voice channel", guild=SERVER_ID)
+@client.tree.command(name="setup", description="Create voice channel", guild=settings.GUILD_ID)
 async def createChannel(interaction: discord.Interaction, name: str):
     try:
         vc = await interaction.guild.create_voice_channel(name)
@@ -54,12 +61,12 @@ async def createChannel(interaction: discord.Interaction, name: str):
 '''
 
 #Delete current channel
-@client.tree.command(name="delete-c", description="Deletes current VC", guild=SERVER_ID)
+@client.tree.command(name="delete-c", description="Deletes current VC", guild=settings.GUILD_ID)
 async def executeCommand(interaction: discord.Interaction):
     await interaction.channel.delete()
 
 #Recruit command
-@client.tree.command(name="recruit", description="Create basic category and channel for a model", guild=SERVER_ID)
+@client.tree.command(name="recruit", description="Create basic category and channel for a model", guild=settings.GUILD_ID)
 async def executeCommand(interaction: discord.Interaction, model_name: str):
     try:
         createdCategory = await interaction.guild.create_category(model_name)
@@ -69,14 +76,14 @@ async def executeCommand(interaction: discord.Interaction, model_name: str):
         await interaction.response.send_message(f"Channel creation failed {e}")
         
 #Test perm command
-@client.tree.command(name="test-perm", description="Tests permission", guild=SERVER_ID)
+@client.tree.command(name="test-perm", description="Tests permission", guild=settings.GUILD_ID)
 @app_commands.checks.has_permissions(manage_channels=True)
-@app_commands.default_permissions(manage_channels=True)
+#@app_commands.default_permissions(manage_channels=True)
 async def executeCommandError(interaction: discord.Interaction):
     await interaction.response.send_message("You have permission!")
 
 #Setup command
-@client.tree.command(name="setup", description="Create voice channel", guild=SERVER_ID)
+@client.tree.command(name="setup", description="Create voice channel", guild=settings.GUILD_ID)
 async def executeCommand(interaction: discord.Interaction):
     try:
         createdChannel = await interaction.guild.create_voice_channel("❌" + interaction.channel.category.name + " - ", category=interaction.channel.category)
@@ -85,7 +92,7 @@ async def executeCommand(interaction: discord.Interaction):
         await interaction.response.send_message(f"Channel creation failed {e}")
 
 #Clean vcs command
-@client.tree.command(name="clean-vc", description="Cleans all VCs", guild=SERVER_ID)
+@client.tree.command(name="clean-vc", description="Cleans all VCs", guild=settings.GUILD_ID)
 async def executeCommand(interaction: discord.Interaction):
     try:
         allChannels = await interaction.guild.fetch_channels()
@@ -97,7 +104,7 @@ async def executeCommand(interaction: discord.Interaction):
         await interaction.response.send_message(f"Failed to delete channel")
         
 #Clean all categories command
-@client.tree.command(name="clean-cat", description="Cleans all categories", guild=SERVER_ID)
+@client.tree.command(name="clean-cat", description="Cleans all categories", guild=settings.GUILD_ID)
 async def executeCommand(interaction: discord.Interaction):
     try:
         allCategories = interaction.guild.categories
@@ -115,23 +122,20 @@ async def executeCommand(interaction: discord.Interaction):
 def is_me(m):
     return m.author == client.user
 
-@client.tree.command(name="purge-msg", description="Clean all messages in current TC", guild=SERVER_ID)
+@client.tree.command(name="purge-msg", description="Clean all messages in current TC", guild=settings.GUILD_ID)
 async def executeCommand(interaction: discord.Interaction):
     deleted = await interaction.channel.purge(limit=100, check=is_me)
     await interaction.response.send_message(f'Deleted {len(deleted)} message(s)')
     
     
-#Error handling
-@executeCommandError.error
-async def info_error(interaction: discord.Interaction, error):
-    print(f"Error type: {type(error)}")
-    if isinstance(error, app_commands.errors.MissingPermissions):
-        await interaction.response.send_message('You do not have a permission.')
+#Error handling locally
+@raiseErrorCommand.error
+async def raiseErrorCommand_handler(interaction: discord.Interaction, error):
+    log.info(f"Error type: {type(error)}")
+    await interaction.response.send_message(f'Error local: {error}')
         
-    
-        
-
-
-
-#Run client
-client.run(BOT_TOKEN)
+   
+#RUN BOT     
+if __name__ == "__main__":
+    #Run client
+    client.run(settings.DISCORD_API_SECRET, root_logger=True)
