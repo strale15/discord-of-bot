d534634b505baf40ad1bb3ceb5213dd883577a69
ERAKOVIC, Strahinja (EXT) <strahinja.erakovic@panonit.com>
2025-03-13 17:46:29 +0100

    Add payment notice

--- schedule.py
+++ schedule.py
@@ -1,4 +1,5 @@
 from logging import Logger
+import string
 import discord
 from discord.ext import tasks
 import datetime
@@ -13,24 +14,36 @@ class Scheduler:
     def __init__(self, bot: commands.Bot, log: Logger):
         self.bot = bot
         self.log = log
-        self.announcement_msg = self.read_announcement_from_file('offDayMsg.txt')
+        self.offday_msg = self.read_announcement_from_file('offDayMsg.txt')
+        self.payment_msg = self.read_announcement_from_file('paymentMsg.txt')
         self.task.start()
 
-        self.last_sent_minute = None
+        self.last_sent_minute_offday = None
+        self.last_sent_minute_payment = None
 
     @tasks.loop(seconds=20)
     async def task(self):
         now = dt.now(timezone)
         current_time = now.strftime("%H:%M")
         
-        if current_time == settings.OFF_DAY_SEND_TIME and current_time != self.last_sent_minute:
-            self.log.info(f"Entered a task at {now}")
-            await self.publish_announcement()
-            self.last_sent_minute = current_time
-        elif current_time != self.last_sent_minute:
-            self.last_sent_minute = None
+        #OffDayRequest
+        if current_time == settings.OFF_DAY_SEND_TIME and now.weekday() == 4 and current_time != self.last_sent_minute_offday:
+            self.log.info(f"Entered a offday task at {now}")
+            await self.publish_announcement(msg=self.offday_msg)
+            self.last_sent_minute_offday = current_time
+        elif current_time != self.last_sent_minute_offday:
+            self.last_sent_minute_offday = None
+            
+        #Payment notice
+        if now.day == 25 and current_time == settings.PAYMENT_SEND_TIME and current_time != self.last_sent_minute_payment:
+            self.log.info(f"Entered a payment task at {now}")
+            await self.publish_announcement(msg=self.payment_msg)
+            self.last_sent_minute_payment = current_time
+        elif current_time != self.last_sent_minute_payment:
+            self.last_sent_minute_payment = None
+            
 
-    async def publish_announcement(self):
+    async def publish_announcement(self, msg: str):
         guild = self.bot.get_guild(settings.ANNOUNCEMENT_GUILD_ID_INT)
 
         if not guild:
@@ -46,7 +59,7 @@ class Scheduler:
             self.log.error("Provided channel is not an announcement channel")
             return
 
-        message_to_publish = await channel.send(self.announcement_msg)
+        message_to_publish = await channel.send(msg)
         await message_to_publish.publish()
 
     def read_announcement_from_file(self, filepath):
